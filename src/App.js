4dac85ab3fae91c5d8788311570f867946fe15ba
konivan <konivan08@gmail.com>
2022-03-08 12:22:14 +0300

    add dispatch

--- src/App.js
+++ src/App.js
@@ -19,15 +19,16 @@ const App = (props) => {
           <Routes>
             <Route
               path="/profile"
-              element={<Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText}/>}
+              element={
+                <Profile
+                  profilePage={props.state.profilePage}
+                  dispatch={props.dispatch}
+                />
+              }
             />
             <Route
               path="/dialogs"
-              element={
-                <Dialogs
-                  state={props.state.dialogsPage}
-                />
-              }
+              element={<Dialogs state={props.state.dialogsPage} />}
             />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
