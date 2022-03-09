841ba583690252c42f2216212b42ab76533b3a7c
konivan <konivan08@gmail.com>
2022-03-09 14:20:39 +0300

    mini refactoring

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -17,4 +17,10 @@ const dialogsReducer = (state, action) => {
   };
 };
 
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+
+export const updateNewMessageBodyCreator = (body) =>
+    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
+
+
 export default dialogsReducer;
--- src/redux/state.js
+++ src/redux/state.js
@@ -2,12 +2,6 @@ import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const ADD_POST = 'ADD-POST';
-
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
-
 let store = {
   _state: {
     profilePage: {
@@ -59,22 +53,5 @@ let store = {
 
   },
 };
-
-export const addPostActionCreator = () => {
-  return {
-    type: ADD_POST,
-  }
-}
-
-export const updateNewPostTextActionCreator = (text) => {
-  return {type: UPDATE_NEW_POST_TEXT, newText: text }
-}
-
-export const sendMessageCreator = () => ({type: SEND_MESSAGE})
-
-export const updateNewMessageBodyCreator = (body) =>
-    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
-
-
 window.store = store;
 export default store;
