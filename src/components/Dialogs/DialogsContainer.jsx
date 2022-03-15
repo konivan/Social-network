146dca0f3a1bfb2fa9c86eae1192a0ff37f302a0
konivan <konivan08@gmail.com>
2022-03-15 21:32:44 +0300

    refactoring

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -11,9 +11,9 @@ let mapStateToProps = (state) => {
 
 let mapDispatchToProps = (dispatch) => {
   return {
-    updateNewMessageBody: () => {dispatch(sendMessageCreator());},
-    sendMessage: (body) => {
-      dispatch(updateNewMessageBodyCreator(body));
+    updateNewMessageBody: (body) => {dispatch(updateNewMessageBodyCreator(body));},
+    sendMessage: () => {
+      dispatch(sendMessageCreator());
     },
   };
 };
