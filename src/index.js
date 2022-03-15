5b363eba23b9cb69d154194406b8e62f8b6ceb0b
konivan <konivan08@gmail.com>
2022-03-15 14:27:27 +0300

    try fix profie

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = (state) => {
+
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
@@ -15,16 +15,7 @@ let rerenderEntireTree = (state) => {
     </React.StrictMode>,
     document.getElementById("root")
   );
-};
 
-rerenderEntireTree(store.getState());
 
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
