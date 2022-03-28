d44c56070d142e9a961a74a586af167a147f6005
konivan <konivan08@gmail.com>
2022-03-28 13:41:10 +0300

    add login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -14,7 +14,7 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 const App = (props) => {
   return (
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
             <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://2code.info/demo/themes/Discy/Main/wp-content/themes/discy/images/logo.png" />
-      <button className={s.loginBtn}>Log In</button>
+      {props.isAuth ? (
+        props.login
+      ) : (
+        <NavLink to={"/login"}>
+          <button className={s.loginBtn}>Log In</button>
+        </NavLink>
+      )}
     </header>
   );
-}
+};
 
 export default Header;
